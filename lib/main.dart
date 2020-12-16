e7fe274ac79fcf7dbc58c551308da993c1f54f99
kiyokawakouji <kohji12k@gmail.com>
2020-12-16 20:24:38 +0900

    ライフサイクルのログを出力する

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,77 @@
 import 'package:flutter/material.dart';
- // main関数から処理が開始され、runAppにWidgetを渡すことでアプリが表示される
-void main() {
-  runApp(MyApp());
-}
- // MyAppはStatelessWidgetを継承しており、Widgetである
- // StatelessWidget(状態を持たない静的なWidget)
- // データが一切変わらない 例：アイコン、ラベル、テキスト...
+import 'package:lifecycle/lifecycle.dart';
+
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
+
   @override
- // WidgetやStateのbuildメソッドの戻り値が、画面に表示されるUIとなる
   Widget build(BuildContext context) {
- // MaterialAppはマテリアルデザインのアプリを作るために便利なWidget
     return MaterialApp(
+      navigatorObservers: [defaultLifecycleObserver],
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-          // MyHomePageはStatefulWidgetを継承しており、状態があることを意味する
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        home: AppLifecycleReactor(title: 'App Lifecycle State'),
+      );
   }
 }
- // StatefulWidget(状態を持つ動的なWidget)
- // ユーザーのインタラクションによってデータが変わる 例：チェックボックス、フォーム、スライダー...
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  final String title;
+class AppLifecycleReactor extends StatefulWidget {
+  const AppLifecycleReactor({ Key key, String title }) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _AppLifecycleReactorState createState() => _AppLifecycleReactorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _AppLifecycleReactorState extends State<AppLifecycleReactor> with WidgetsBindingObserver {
+  String text = '';
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
     setState(() {
-
-      _counter++;
+      text = state.toString();
     });
+   print(state);
+
+    if(state == AppLifecycleState.resumed){
+    }else if(state == AppLifecycleState.inactive){
+    }else if(state == AppLifecycleState.paused){
+    }else if(state == AppLifecycleState.detached){
+    }
   }
 
   @override
   Widget build(BuildContext context) {
- // Scaffoldは典型的な画面レイアウトを構築する便利なWidget
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              '',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+              "$text",
+            )
+          ]
+        )
+      )
     );
   }
 }
+
+
+
+
+
